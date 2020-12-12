7eb54fc0aa5f5a048b7e9611c6c1841d8f8282a0
clemna <1614792256@qq.com>
2020-12-12 09:25:45 +0800

    原版本

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,3 +1,4 @@
+// pages/category/category.js
 Page({
 
   /**
@@ -5,81 +6,61 @@ Page({
    */
   data: {
 
-    bei:[
-      {
-        title:'去北湖南路拿快递',
-        recordTime:'12月2日',
-        remindTime:'12月5日 12：00',
-      },
-      {
-        title: '完成金工实习报告',
-        recordTime: '12月2日',
-        remindTime: '12月4日 20：00',
-      },
-      {
-        title: '完成毛概作业',
-        recordTime: '12月5日',
-        remindTime: '12月31日 20：00',
-      }
-      ],
-
   },
-
-
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
