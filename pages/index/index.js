3c117ce2d16ea4b6340dc87c9b98412e019af648
clemna <1614792256@qq.com>
2020-12-13 16:05:19 +0800

    初始文件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,37 +1,109 @@
+var app = getApp
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    
+    showModalStatus: false,  //下方弹出层初始值：false
+    animationData: '',
 
-    bei:[
+    bei: [
       {
-        title:'去北湖南路拿快递',
-        recordTime:'12月2日',
-        remindTime:'12月5日 12：00',
+        title: '去北湖南路拿快递',
+        recordtime: '12月2日',
+        remindtime: '12月5日 12：00',
+        isCollect: false, // 备忘录收藏功能：默认没有收藏
       },
       {
         title: '完成金工实习报告',
-        recordTime: '12月2日',
-        remindTime: '12月4日 20：00',
+        recordtime: '12月2日',
+        remindtime: '12月4日 20：00',
+        isCollect: false, // 备忘录收藏功能：默认没有收藏
       },
       {
         title: '完成毛概作业',
-        recordTime: '12月5日',
-        remindTime: '12月31日 20：00',
+        recordtime: '12月5日',
+        remindtime: '12月31日 20：00',
+        isCollect: false, // 备忘录收藏功能：默认没有收藏
+      },
+      {
+        title: '吃饭',
+        recordtime: '12月5日',
+        remindtime: '12月31日 20：00',
+        isCollect: false, // 备忘录收藏功能：默认没有收藏
+      },
+      {
+        title: '睡觉',
+        recordtime: '12月5日',
+        remindtime: '12月31日 20：00',
+        isCollect: false, // 备忘录收藏功能：默认没有收藏
+      }, 
+      {
+        title: '玩游戏',
+        recordtime: '12月5日',
+        remindtime: '12月31日 20：00',
+        isCollect: false, // 备忘录收藏功能：默认没有收藏
       }
-      ],
-
-  },
 
 
 
+
+
+    ],
+
+
+   },
+
+
+ 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function () {
+
+    },
+
+
+  // 点击收藏事件
+  toCollect: function (res) {
+    var fav_id = res.currentTarget.dataset.index; //获取备忘录定位
+    var bol = this.data.bei[fav_id].isCollect;
+
+    this.setData({
+      [`bei[${fav_id}].isCollect`]: !bol  // 改变状态
+    })
+
+  },
+
+   // 显示遮罩层
+  showModal: function () {
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "ease-in-out",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(500).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+
+  // 隐藏弹出层
+  hideModal: function () {
+    this.setData({
+      showModalStatus: false,
+    })
+
   },
 
   /**
@@ -81,5 +153,6 @@ Page({
    */
   onShareAppMessage: function () {
     
-  }
+  },
+
 })
